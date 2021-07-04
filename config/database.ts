e92ca1c4896a69e0eea05d1e443763869bd37e30
arifinmn11 <nurarifinmuhammad@gmail.com>
2021-07-04 20:52:57 +0700

    deploy

--- config/database.ts
+++ config/database.ts
@@ -68,7 +68,7 @@ const databaseConfig: DatabaseConfig = {
         port: Env.get('PG_PORT'),
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
+        database: Env.get('PG_DB_NAME')
       },
       migrations: {
         naturalSort: true,
